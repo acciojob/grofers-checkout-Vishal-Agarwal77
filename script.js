2923fc6f25a56cbaf22e7fa2060150f7adca8809
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-25 01:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const prices=document.getElementsByClassName("price");
+	let ans=0;
+	for(let i of prices){
+		ans+=Number(i.innerText);
+	}
+	// console.log(ans);
+	let row=document.createElement("tr");
+	let col1=document.createElement("td");
+	let col2=document.createElement("td");
+	col2.innerText=`${ans}`;
+	row.appendChild(col1);
+	row.appendChild(col2);
+	let table=document.getElementsByTagName("table")[0];
+	table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
